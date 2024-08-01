d11aab07c4c07ab27a723b178c2cb26f1c96c69d
Artur Prata <artur.pigari@gmail.com>
2024-08-01 18:44:37 -0300

    Inicialização do Projeto em Laravel

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Gres\'s Anatomy'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $series) {
+            $html .= "<li>$series</li>";
+        }
+        $html .= '</ul>';
+
+        return $html;
+    }
+}
